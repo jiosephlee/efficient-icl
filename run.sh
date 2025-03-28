7e6022c8eaaf1c9e22ac3b7d9639beedbdcd6be1
Joseph Lee <jiosephlee@gmail.com>
2025-03-28 00:13:00 -0400

    updated run.sh file

--- run.sh
+++ run.sh
@@ -1,3 +1,7 @@
-MODEL="llama_3.1_8b_instruct"
-VERSION="v0"
-nohup python notebooks+scripts/grpo.py --mode evaluate  --dataset gsm8k > v0_0326.out 2>&1 &
+MODEL="meta-llama/meta-Llama-3.1-8B-Instruct"
+MODEL_NAME="meta-Llama-3.1-8B-Instruct"
+DATASET="gsm8k"
+MODE="evaluate"
+LORA_NAME="v1"
+DATE=$(date +"%Y%m%d_%H%M")
+nohup python python train_grpo.py --model ${MODEL} --mode ${MODE} --dataset ${DATASET} --lora_name ${LORA_NAME} > ${MODEL_NAME}_${LORA_NAME}_${MODE}_${DATASET}_${DATE}.out 2>&1 &
